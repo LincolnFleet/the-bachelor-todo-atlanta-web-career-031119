3641bb8e50f41360104dfc4b9990f6d5637ebd9d
github username <github email address>
2019-07-06 18:23:50 -0400

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,23 @@
+require 'byebug'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner = data[season].map do |item|
+    if item['status'] == 'Winner'
+      x=item['name'].split(' ')
+      return x[0]
+    else
+    end
+  end
+  return winner||'Season has not concluded'
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  res= data.reduce([]) do |acc, season|
+    matches=season[1].select{|y| y['occupation']==occupation}
+    names=matches.map{|z| z['name']}
+    acc+names
+  end
+  res.join(',')
 end
 
 def count_contestants_by_hometown(data, hometown)
